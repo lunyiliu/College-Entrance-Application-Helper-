6b1425e6756ceb34e89f0e856718cf65e689112c
lunyiliu <93286784@qq.com>
2019-03-14 10:05:38 +0800

    2019 3.14 lunyiliu

---
+++ 高考志愿程序_后端/前期数据爬取（往年分数线等）/高考网专业表学校补全.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 23 22:51:59 2018
+
+@author: lenovvo
+"""
+
+import requests
+from lxml import etree
+import time
+import re
+import pymysql
+#必须要能读出括号
+def get_text(content):
+    content_text=[]
+    for con in content:
+        content_text_son=[]
+        re_obj1=re.search(u"[\u4e00-\u9fa5]+、?[\u4e00-\u9fa5]+",con)#专业名称
+        content_text_son.append(re_obj1.group())
+        con=con[re_obj1.span()[1]:-1]+con[-1]
+        re_obj2=re.search(u"[\u4e00-\u9fa5]+[（]?[(]?-?[\u4e00-\u9fa5]+[)]?[）]?",con)#大学名称
+        content_text_son.append(re_obj2.group())
+        con=con[re_obj2.span()[1]:-1]+con[-1]
+        re_obj3=re.search("([0-9]+)|(-+)",con)#平均分
+        content_text_son.append(re_obj3.group())
+        con=con[re_obj3.span()[1]:-1]+con[-1]
+        re_obj4=re.search("([0-9]+)|(-+)",con)#最高分
+        content_text_son.append(re_obj4.group())
+        con=con[re_obj4.span()[1]:-1]+con[-1]
+        re_obj5=re.search(u"[\u4e00-\u9fa5]+",con)#省份
+        content_text_son.append(re_obj5.group())
+        con=con[re_obj5.span()[1]:-1]+con[-1]
+        re_obj6=re.search(u"[\u4e00-\u9fa5]+",con)#科类
+        content_text_son.append(re_obj6.group())
+        con=con[re_obj6.span()[1]:-1]+con[-1]
+        re_obj7=re.search("[0-9]+",con)#年份
+        content_text_son.append(re_obj7.group())
+        con=con[re_obj7.span()[1]:-1]+con[-1]
+        re_obj8=re.search(u"[\u4e00-\u9fa5]+",con)#批次
+        content_text_son.append(re_obj8.group())
+        con=con[re_obj8.span()[1]:-1]+con[-1]
+        content_text.append(content_text_son)
+    return content_text
+school_names=['中国石油大学','中国地质大学','中国矿业大学','北京师范大学','中国人民大学']
+conn = pymysql.connect(host='39.96.168.183', user='root', passwd='123456', db='gaokao', charset='utf8')
+cursor=conn.cursor(cursor=pymysql.cursors.DictCursor)
+cursor.execute('select 学校名称 from major_supplement')
+conn.commit()
+school_supp=cursor.fetchall()
+conn.close()
+for dic in school_supp:
+    school_names.append(dic['学校名称'])
+flag_province=1
+flag_kelei=1
+flag_year=2012
+flag_page=1
+school_names=['中国矿业大学(徐州)','北京师范大学-香港浸会大学联合国际学院']
+for provinceID in range(flag_province,32):
+    if provinceID!=flag_province:
+        flag_kelei=1
+    for keleiID in range(flag_kelei,3):
+        if keleiID!=flag_kelei:
+            flag_year=2012
+        for year in range(flag_year,2018):
+            if year!=flag_year:
+                flag_page=1
+            page=flag_page
+            for school_name in school_names:
+                while 1:
+                    url="http://college.gaokao.com/spepoint/a"+str(provinceID)+"/s"+str(keleiID)+"/y"+str(year)+"/o"+school_name+"/p"+str(page)+"/" 
+                    page=page+1
+                    while 1: 
+                        try:
+                            r=requests.get(url,timeout=3.05)
+                            break
+                        except Exception as e:
+                            print("Exception: {}".format(e))
+                            time.sleep(5)
+                    selector=etree.HTML(r.text)
+                    #“sz”class
+                    content1_=selector.xpath("//tr[@class='sz']")
+                    #“szw” class
+                    content2_=selector.xpath("//tr[@class='szw']")
+                    if content1_==[] and content2_==[]:
+                        break
+                    content1=[]
+                    for c in content1_:
+                        content1.append(c.xpath('string(.)').strip())
+    
+                    content2=[]
+                    for c in content2_:
+                        content2.append(c.xpath('string(.)').strip())
+                    try:
+                        content_text1=get_text(content1)
+                        content_text2=get_text(content2)
+                    
+                    except Exception as e:
+                        print('正当'+school_name+"时遇到Exception: {}".format(e))
+                        continue
+                    
+                    content_text=content_text1+content_text2
+                    conn = pymysql.connect(host='39.96.168.183', user='root', passwd='123456', db='gaokao', charset='utf8')
+                    cursor=conn.cursor(cursor=pymysql.cursors.DictCursor)
+                    for con_text in content_text:
+                        if con_text[2]=='--':
+                            con_text[2]='Null'
+                        if con_text[3]=='--':
+                            con_text[3]='Null'
+                        try:
+                            sql="insert into majors_copy values('"+str(con_text[0])+"','"+str(con_text[1])+"',"+str(con_text[2])+', Null ,'+str(con_text[3])+",'"+str(con_text[4])+"','"+str(con_text[5])+"',"+str(con_text[6])+",'"+str(con_text[7])+"' , Null , Null , Null , Null)"
+                            cursor.execute(sql)
+                            conn.commit()
+                        except Exception as e:
+                            #print("在"+con_text[1]+con_text[0]+"遇到Exception: {}".format(e))
+                            with open ('err_log.txt','a+',encoding='utf-8') as f:
+                                f.writelines("在"+con_text[1]+con_text[0]+"遇到Exception: {}\n".format(e))
+                                f.close()
+                            continue
+                    print("第"+str(provinceID)+"个省"+"第"+str(year)+"年"+"第"+str(page-1)+"页，科类"+str(keleiID))
+                    conn.close()
