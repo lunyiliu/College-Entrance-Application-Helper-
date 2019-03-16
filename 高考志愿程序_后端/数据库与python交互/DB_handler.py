d8854be42c05d931a9a72a3231c54f9a426018ea
lunyiliu <93286784@qq.com>
2019-03-16 21:35:58 +0800

    3.16 lunyiliu

--- 高考志愿程序_后端/数据库与python交互/DB_handler.py
+++ 高考志愿程序_后端/数据库与python交互/DB_handler.py
@@ -11,7 +11,7 @@ class DB_handler:
     def __init__(self):
         self.conn = pymysql.connect(host='39.96.168.183', user='root', passwd='123456', db='gaokao', charset='utf8')
         self.cursor=self.conn.cursor()
-    def select(self,FormList:"list，从哪几个表中选",ResultList:"list，需要返回哪几个字段的结果",ConditionList:"列表，查询的条件，列表元素格式：XX=XX",para_logic:"哪些条件需要或逻辑"=[],para_OrderBy:"第一个元素，排序的字段，第二个元素，true为降序，无为升序"=[],para_dict:"这个参数是true的话，返回的结果是pymysql默认的字典"=False,para_distinct=False,para_limitation:"限制返回的参数个数"=0,para_debug:'此参数开启将打印出sql语句'=False)->"返回的结果是这样的：[[字段1的所有结果],[字段2的所有结果]]":
+    def select(self,FormList:"list，从哪几个表中选",ResultList:"list，需要返回哪几个字段的结果",ConditionList:"列表，查询的条件，列表元素格式：XX=XX",para_logic:"哪些条件需要或逻辑"=[],para_OrderBy:"第一个元素，排序的字段，第二个元素，true为降序，无为升序"=[],para_dict:"这个参数是true的话，返回的结果是pymysql默认的字典"=False,para_distinct=False,para_limitation:"限制返回的参数个数"=0,para_debug:'此参数开启将打印出sql语句'=False)->"返回的结果是这样的：[[字段1的所有结果],[字段2的所有结果]]，如果字段i只有一条结果的时候，列表将没有嵌套，如果只有一个字段且该字段只有一条结果，则输出一个值":
         if para_dict==True:
             self.cursor=self.conn.cursor(cursor=pymysql.cursors.DictCursor)
         sql="select "
@@ -47,15 +47,21 @@ class DB_handler:
             return r_
         if len(ResultList)!=1:
             Result=[]
-            for i in range(len(ResultList)):
-                Result.append([])
-            for i in range(len(r_)):
-                for j in range(len(r_[i])):
-                    Result[j].append(r_[i][j])
+            if len(r_)!=1:#如果字段i的结果只有一条的话，没有必要嵌套一层列表
+                for i in range(len(ResultList)):
+                    Result.append([])
+                for i in range(len(r_)):
+                    for j in range(len(r_[i])):
+                        Result[j].append(r_[i][j])
+            else:
+                Result=list(r_[0])
         else:
-            Result=[]
-            for i in range(len(r_)):
-                Result.append(r_[i][0])
+            if len(r_)!=1:#如果字段只有一个且其结果只有一条，只返回一个值
+                Result=[]
+                for i in range(len(r_)):
+                    Result.append(r_[i][0])
+            else:
+                Result=r_[0][0]
         return Result
     def insert(self,form:"要插入的表，str",values:"list，可以是批量（建议批量插入）[[xx，xx，...]，[xx，xx，...]，...]，也可以是一条[xx，xx，xx]"):
         sql="insert into %s values ("%(form)
