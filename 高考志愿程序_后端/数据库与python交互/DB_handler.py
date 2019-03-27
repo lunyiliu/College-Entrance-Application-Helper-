f7bb7a417ce6508384e0ede9a0931d52613a471d
Lemburse <40856911+Lemburse@users.noreply.github.com>
2019-03-27 23:32:28 +0800

    Update DB_handler.py

--- 高考志愿程序_后端/数据库与python交互/DB_handler.py
+++ 高考志愿程序_后端/数据库与python交互/DB_handler.py
@@ -10,13 +10,14 @@ class DB_handler:
     def __init__(self):
         self.conn = pymysql.connect(host='39.97.100.184', user='root', passwd='8612260', db='gaokao', charset='utf8')
         self.cursor=self.conn.cursor()
-    def select(self,FormList:"list，从哪几个表中选",ResultList:"list，需要返回哪几个字段的结果",ConditionList:"列表，查询的条件，列表元素格式：XX=XX",para_logic:"哪些条件需要或逻辑"=[],para_OrderBy:"第一个元素，排序的字段，第二个元素，true为降序，无为升序"=[],para_dict:"这个参数是true的话，返回的结果是pymysql默认的字典"=False,para_distinct=False,para_limitation:"限制返回的参数个数"=0,para_debug:'此参数开启将打印出sql语句'=False)->"返回的结果是这样的：[[字段1的所有结果],[字段2的所有结果]]，如果字段i只有一条结果的时候，列表将没有嵌套，如果只有一个字段且该字段只有一条结果，则输出一个值":
+    def select(self,FormList:"list，从哪几个表中选",ResultList:"list，需要返回哪几个字段的结果",ConditionList:"列表，查询的条件，列表元素格式：XX=XX",para_logic:"哪些条件需要或逻辑,[[xx,value1,value2,...]],[xx,[]]]"=[],para_OrderBy:"第一个元素，排序的字段，第二个元素，true为降序，无为升序"=[],para_dict:"这个参数是true的话，返回的结果是pymysql默认的字典"=False,para_distinct=False,para_limitation:"限制返回的参数个数"=0,para_debug:'此参数开启将打印出sql语句'=False)->"返回的结果是这样的：[[字段1的所有结果],[字段2的所有结果]]，如果字段i只有一条结果的时候，列表将没有嵌套，如果只有一个字段且该字段只有一条结果，则输出一个值":
+        #必须由condiyionlist才有para——logic 有点蠢后续在该
         if para_dict==True:
             self.cursor=self.conn.cursor(cursor=pymysql.cursors.DictCursor)
         sql="select "
         requests = []
         val = []
-        mid={}
+        mid ={}
         if para_distinct==True:
             sql+="distinct "
         for result in ResultList:
@@ -34,24 +35,36 @@ class DB_handler:
                      #print(condition.split('=')[1].strip())
                      val.append(condition.split('=')[1].strip())
                 else:
-                     condition=condition.strip()    
+                     condition=condition.strip()
                      request=condition.split(' ')[0].strip()
                      requests.append(request)
                      #print(condition.split('=')[1].strip())
                      mid[request]=condition.split(' ')[1].strip()
                      if mid[request] != 'is':
                          val.append(condition.split(' ')[2].strip())
-                     
+
             if para_logic==[]:
                 for request in requests:
-                    if request not in mid.keys():
-                        sql+=str(request)+'= %s'+" and "
-                    else:
-                        if mid[request] !='is':
-                            sql+=str(request)+' '+mid[request]+' %s'+" and "
-                        else :
-                            sql+=str(request)+" is Null and "
-            sql=sql.strip(" and ")
+                    sql+=str(request)+'= %s'+" and "
+                sql = sql.strip(" and ")
+            else:
+                for request in requests:
+                    sql+=str(request)+'= %s'+" and "
+                for logic in para_logic:
+                    sql = sql +'('
+                    column = logic[0] #栏字段名
+                    for i in range(len(logic)-1):
+                        sql = sql + column + '= %s' + ' or '
+                        val.append(logic[i+1])
+                    sql = sql.strip(' or ')
+                    sql = sql + ')'
+                    sql = sql+' and '
+                sql = sql.strip(' and ')
+        print(val)
+        #print(type(val[1]))
+
+
+
         if para_limitation!=0:
             sql+=" limit "+str(para_limitation)
         if para_OrderBy!=[]:
@@ -125,9 +138,8 @@ class DB_handler:
                     sql+=str(request)+'= %s'+" and "
             sql=sql.strip(" and ")
         #sql = sql+';'
-        print(sql)
-        print(val)
+        #print(sql)
+        #print(val)
         self.cursor.execute(sql,val)
         self.conn.commit()
-                
                 
