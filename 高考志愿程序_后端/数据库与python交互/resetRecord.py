0ad4ec8d92a485999482e3cf90445c77b065fe39
Lemburse <40856911+Lemburse@users.noreply.github.com>
2019-04-11 19:11:53 +0800

    Update resetRecord.py

--- 高考志愿程序_后端/数据库与python交互/resetRecord.py
+++ 高考志愿程序_后端/数据库与python交互/resetRecord.py
@@ -6,18 +6,21 @@ class resetRecords():
         self.DBh = DB_handler()
         self.DBm = DB_methods()
         self.user = user_ID
-        self.score = str(self.DBh.select(['client'], ['score'], ['user_ID =' + user_ID]))
-        self.provinceID = str(self.DBh.select(['client'], ['provinceID'], ['user_ID =' + user_ID]))
-        self.subject = self.DBh.select(['client'], ['subject'], ['user_ID =' + user_ID])
-        self.year = str(self.DBh.select(['client'], ['year'], ['user_ID =' + user_ID]))
-        self.pici = str(self.DBh.select(['client'], ['pici'], ['user_ID =' + user_ID]))
+        store = self.DBh.select(['client'], ['score','province','subject','year','pici','choose_list'], ['user_ID =' + user_ID])
+        self.score = str(store[0])
+        self.provinceID = str(store[1])
+        #print(self.provinceID)
+        self.subject = store[2]
+        self.year = str(store[3])
+        self.pici = str(store[4])
         self.intension_list = []
-        self.mid_list = self.DBh.select(['client'], ['choose_list'], ['user_ID =' + user_ID])[:-1].split(',')
+        self.mid_list = store[5][:-1].split(',')
+        #print(self.mid_list)
         for element in self.mid_list:
             self.intension_list.append(element[:-1].split('.'))
-
+        #print(self.intension_list)
         self.intension_list_new = []
-        self.mid_list_new = self.DBh.select(['client'], ['choose_list'], ['user_ID =' + user_ID])[:-1].split(',') #可能会有改动client换成记录表名字
+        self.mid_list_new = store[5][:-1].split(',') #可能会有改动client换成记录表名字
         for element in self.mid_list_new:
             self.intension_list_new.append(element[:-1].split('.'))
 
@@ -133,7 +136,7 @@ class resetRecords():
 
             #对每个学校的每个专业进行修改：
             zhuanye_list = []
-            for k in range(len(self.intension_list[0])-1):
+            for k in range(len(self.intension_list[j])-1):
                 k1 = k+1
                 zhuanye_list.append(self.intension_list[j][k1]) #记录某学校的专业选择情况
             for l in range(len(zhuanye_list)):#对每个专业
@@ -248,18 +251,20 @@ class resetRecords():
             school_list.append(self.intension_list[i][0])
         for j in range(len(school_list)):
             school_str = str(school_list[j])
-
+            #print(school_list)
             serial = str(j + 1)
             Conditionlist = ['schoolID =' + school_str, 'provinceID =' + self.provinceID,
                              'subject ='  + self.subject , 'pici =' + self.pici, 'serialnumber =' + serial]
 
             # 先对学校进行改信息
             if int(self.score) < 430:
-                store_pre = self.DBh.select(['intension_school_table'], ['block1'], Conditionlist)
-                if store_pre == None or store_pre == '':
+                #print('yea')
+                store_pre = self.DBh.select(['intention_school_table'], ['block1'], Conditionlist)
+                if store_pre is None or store_pre == ''or store_pre == []:
                     count = '1'
                     result_str = count+','+self.score+'.'
                 else:
+                    #print(store_pre)
                     store = store_pre.split(',')[1][:-1].split('.')  # 记录的列表
                     count = store_pre.split(',')[0]  # 记录人数
 
@@ -275,11 +280,11 @@ class resetRecords():
                     result_str = count + ','
                     for element in store:
                         result_str = result_str + element + '.'
-                self.DBh.update('intension_school_table', ['block1 =' + '\"' + result_str + '\"'], Conditionlist)
+                self.DBh.update('intention_school_table', ['block1 =' + '\"' + result_str + '\"'], Conditionlist)
 
             elif int(self.score) < 510:
-                store_pre = self.DBh.select(['intension_school_table'], ['block2'], Conditionlist)
-                if store_pre == None or store_pre == '':
+                store_pre = self.DBh.select(['intention_school_table'], ['block2'], Conditionlist)
+                if store_pre is None or store_pre == '' or store_pre == []:
                     count = '1'
                     result_str = count + ',' + self.score + '.'
                 else:
@@ -297,11 +302,12 @@ class resetRecords():
                     result_str = count + ','
                     for element in store:
                         result_str = result_str + element + '.'
-                self.DBh.update('intension_school_table', ['block2 =' + '\"' + result_str + '\"'], Conditionlist)
+                self.DBh.update('intention_school_table', ['block2 =' + '\"' + result_str + '\"'], Conditionlist)
 
             elif int(self.score) < 590:
-                store_pre = self.DBh.select(['intension_school_table'], ['block3'], Conditionlist)
-                if store_pre == None or store_pre == '':
+                store_pre = self.DBh.select(['intention_school_table'], ['block3'], Conditionlist)
+                #print(store_pre is None)
+                if store_pre is None or store_pre == ''or store_pre == []:
                     count = '1'
                     result_str = count + ',' + self.score + '.'
                 else:
@@ -319,11 +325,11 @@ class resetRecords():
                     result_str = count + ','
                     for element in store:
                         result_str = result_str + element + '.'
-                self.DBh.update('intension_school_table', ['block3 =' + '\"' + result_str + '\"'], Conditionlist)
+                self.DBh.update('intention_school_table', ['block3 =' + '\"' + result_str + '\"'], Conditionlist)
 
             elif int(self.score) < 670:
-                store_pre = self.DBh.select(['intension_school_table'], ['block4'], Conditionlist)
-                if store_pre == None or store_pre == '':
+                store_pre = self.DBh.select(['intention_school_table'], ['block4'], Conditionlist)
+                if store_pre is None or store_pre == '' or store_pre == []:
                     count = '1'
                     result_str = count + ',' + self.score + '.'
                 else:
@@ -341,11 +347,11 @@ class resetRecords():
                     result_str = count + ','
                     for element in store:
                         result_str = result_str + element + '.'
-                self.DBh.update('intension_school_table', ['block4 =' + '\"' + result_str + '\"'], Conditionlist)
+                self.DBh.update('intention_school_table', ['block4 =' + '\"' + result_str + '\"'], Conditionlist)
 
             elif int(self.score) < 750:
-                store_pre = self.DBh.select(['intension_school_table'], ['block5'], Conditionlist)
-                if store_pre == None or store_pre == '':
+                store_pre = self.DBh.select(['intention_school_table'], ['block5'], Conditionlist)
+                if store_pre is None or store_pre == '' or store_pre == []:
                     count = '1'
                     result_str = count + ',' + self.score + '.'
                 else:
@@ -363,13 +369,14 @@ class resetRecords():
                     result_str = count + ','
                     for element in store:
                         result_str = result_str + element + '.'
-                self.DBh.update('intension_school_table', ['block5 =' + '\"' + result_str + '\"'], Conditionlist)
+                self.DBh.update('intention_school_table', ['block5 =' + '\"' + result_str + '\"'], Conditionlist)
 
             # 对每个学校的每个专业进行修改：
             zhuanye_list = []
             for k in range(len(self.intension_list[0]) - 1):
                 k1 = k + 1
-                zhuanye_list.append(self.intension_list[j][k1])  # 记录某学校的专业选择情况
+                #print(k1)
+                zhuanye_list.append(self.intension_list[j][k1])  # 记录j+1学校的专业选择情况
             for l in range(len(zhuanye_list)):  # 对每个专业
                 serial = str(l + 1)
                 zhuanye_str = zhuanye_list[l]
@@ -380,7 +387,7 @@ class resetRecords():
 
                 if int(self.score) < 430:
                     store_pre = self.DBh.select(['intension_zhuanye_table'], ['block1'], Conditionlist)
-                    if store_pre == None or store_pre == '':
+                    if store_pre == None or store_pre == '' or store_pre == []:
                         count = '1'
                         result_str = count + ',' + self.score + '.'
                     else:
@@ -403,7 +410,7 @@ class resetRecords():
 
                 elif int(self.score) < 510:
                     store_pre = self.DBh.select(['intension_zhuanye_table'], ['block2'], Conditionlist)
-                    if store_pre == None or store_pre == '':
+                    if store_pre == None or store_pre == ''or store_pre == []:
                         count = '1'
                         result_str = count + ',' + self.score + '.'
                     else:
@@ -426,7 +433,7 @@ class resetRecords():
 
                 elif int(self.score) < 590:
                     store_pre = self.DBh.select(['intension_zhuanye_table'], ['block3'], Conditionlist)
-                    if store_pre == None or store_pre == '':
+                    if store_pre == None or store_pre == ''or store_pre == []:
                         count = '1'
                         result_str = count + ',' + self.score + '.'
                     else:
@@ -449,7 +456,7 @@ class resetRecords():
 
                 elif int(self.score) < 670:
                     store_pre = self.DBh.select(['intension_zhuanye_table'], ['block4'], Conditionlist)
-                    if store_pre == None or store_pre == '':
+                    if store_pre == None or store_pre == ''or store_pre == []:
                         count = '1'
                         result_str = count + ',' + self.score + '.'
                     else:
@@ -472,7 +479,7 @@ class resetRecords():
 
                 elif int(self.score) < 750:
                     store_pre = self.DBh.select(['intension_zhuanye_table'], ['block5'], Conditionlist)
-                    if store_pre == None or store_pre == '':
+                    if store_pre == None or store_pre == ''or store_pre == []:
                         count = '1'
                         result_str = count + ',' + self.score + '.'
                     else:
